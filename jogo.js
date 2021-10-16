b0d5905ea1532044fa1f2d60f21b66eff55adb86
Marcos Royer <marcosoliveiraroyer@gmail.com>
2021-10-16 17:07:18 -0300

    Gravidade do passarinho

--- jogo.js
+++ jogo.js
@@ -72,6 +72,12 @@ const flappyBird = {
     altura: 24,
     x: 10,
     y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    atualiza(){
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade,
+        flappyBird.y = flappyBird.y + flappyBird.velocidade;
+    },
     desenha(){
         contexto.drawImage(
             sprites, //imagem
@@ -86,10 +92,12 @@ const flappyBird = {
 
 function loop(){
     
-    
+    flappyBird.atualiza();
     planoDeFundo.desenha();
     chao.desenha();
     flappyBird.desenha();
+
+    
 
     requestAnimationFrame(loop) //função para otimização de desenho dos quadros
 }
